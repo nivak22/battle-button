feddd34d761ca481cf85f001fca82420d0695335
nivak22 <90485437+nivak22@users.noreply.github.com>
2025-01-08 19:21:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
 const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path'); // Asegúrate de que esta línea esté presente
+const { v4: uuidv4 } = require('uuid');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
